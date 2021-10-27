6451830551fba5475f8073bd8e998bde59f839d9
pooneh <you@example.com>
2021-10-26 23:05:06 -0700

    first commit

---
+++ http_collector.js
@@ -0,0 +1,62 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    const results = []; // collects all results
+
+    let paused = false;
+    let pausedRequests = [];
+
+    const nextRequest = () => { // continue the next request or "unpause"
+        if (pausedRequests.length === 0) {
+            paused = false;
+        } else {
+            // continue first request in "queue"
+            (pausedRequests.shift())(); // calls the request.continue function
+        }
+    };
+
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+        if (paused) {
+            pausedRequests.push(() => request.continue());
+        } else {
+            paused = true; // pause, as we are processing a request now
+            request.continue();
+        }
+    });
+
+    page.on('requestfinished', async (request) => {
+        const response = await request.response();
+
+        const responseHeaders = response.headers();
+        let responseBody;
+        if (request.redirectChain().length === 0) {
+            // body can only be access for non-redirect responses
+            responseBody = await response.buffer();
+        }
+
+        const information = {
+            url: request.url(),
+            requestHeaders: request.headers(),
+            requestPostData: request.postData(),
+            responseHeaders: responseHeaders,
+            responseSize: responseHeaders['content-length'],
+            responseBody,
+        };
+        results.push(information);
+
+        nextRequest(); // continue with next request
+    });
+    page.on('requestfailed', (request) => {
+        // handle failed request
+        nextRequest();
+    });
+
+    await page.goto('https://www.facebook.com', { waitUntil: 'networkidle0' });
+    console.log(results);
+    console.log(results.length)
+    await browser.close();
+})();
